8aece11bb13caf445d51583ce27520cd473086af
rafin8990 <rafin8990@gmail.com>
2023-05-03 12:00:49 +0600

    VIDEO TWO COMPLETED

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const isAdmin:boolean=true;
 
 course="programming hero web development";
 
+let play:string;
+
+play='cricket'
+
 
 /* 
 ***premitive data type in type script
@@ -18,4 +22,17 @@ course="programming hero web development";
 
 /* 
 টাইপস্ক্রিপ্ট এ যেহেতু নামের সাথে ভেলু এড করে দেওয়া হয় এজন্য নাম্বার টাইপ ডাটার ভেতরে কখনো স্ট্রিং টাইপ ডেটা এসাইন করা যাবে না । অথবা কোন স্ট্রিং টাইপ ডেটার ভেতরে কোন নাম্বার বা ‍ুলিয়ান ভেলু এড করা যাবে না । 
+*/
+
+
+
+/* 
+টাইপস্ক্রিপ্ট এ এনি টাইপ ভেলু লেখার সময় এর সাথে ওই ভেরিয়েবল কি টাইপ হবে সেটা উল্লেখ করে দিতে হয় । 
+যেমন: let value; এর পরিবর্তে let value:numer; লিখতে হয় । 
+*/
+
+
+
+/* 
+টাইপসি্ক্রপ্টের এনি টাইপ ভেলু যদি undefined  হয় তাহলে তার জন্য অবশ্যই const use করতে হবে । let ইউজ করা যাবে না । 
 */
